597381a2020cf272bf787f44500fdb5ccff51883
Lviana21 <lucianoviana12@gmail.com>
2021-06-01 06:58:38 -0300

    Utilizando método Math

---
+++ src/urionline/iniciante/MetodoMath.java
@@ -0,0 +1,21 @@
+package urionline.iniciante;
+
+public class MetodoMath {
+
+	public static void main(String[] args) {
+
+		System.out.println(Math.min(5, 10)); // Retorna o menor valor
+		System.out.println(Math.max(20, 30)); // Retorna o maior valor
+		System.out.println(Math.abs(-22)); // Retorna o valor absoluto
+		System.out.println(Math.sqrt(16)); // Retorna a raiz quadrada de um número
+		System.out.println(Math.pow(4, 2)); // Retorna a pontencia do número 4 elevado a 4
+
+		System.out.println(Math.round(10.5)); // Arredonda para cima ou para baixo (0.5 para cima, 0.4 para baixo)
+		System.out.println(Math.ceil(30.2)); // Arredonda para cima
+		System.out.println(Math.floor(40.6)); // Arredonda para baixo
+
+		System.out.println(Math.random()); // Gera um número aleatório
+
+	}
+
+}
