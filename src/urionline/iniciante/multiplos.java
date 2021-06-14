893d4914b219af35fe6b817ef5ad956a7d65ab59
Luciano Viana <lucianoviana12@gmail.com>
2021-06-14 08:16:35 -0300

    Exercício Resolvido: 1044

---
+++ src/urionline/iniciante/multiplos.java
@@ -0,0 +1,36 @@
+package urionline.iniciante;
+
+import java.util.Scanner;
+
+/*
+#URI 1043 - MÚLTIPLOS
+Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" 
+ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si.
+
+ENTRADA
+A entrada contém valores inteiros.
+
+SAÍDA
+A saída deve conter uma das mensagens conforme descrito acima.
+
+*/
+
+public class multiplos {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		int a = input.nextInt();
+		int b = input.nextInt();
+
+		if (a % b == 0 || b % a == 0) {
+			System.out.println("Sao Multiplos");
+		} else {
+			System.out.println("Nao sao Multiplos");
+		}
+
+		input.close();
+	}
+
+}
