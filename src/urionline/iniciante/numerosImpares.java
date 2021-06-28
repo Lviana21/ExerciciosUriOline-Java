c1002b37e5ce50834b2d0ed0682db0f2867d4a82
Luciano Viana <lucianoviana12@gmail.com>
2021-06-28 07:22:42 -0300

    Exercício Resolvido: 1067

---
+++ src/urionline/iniciante/numerosImpares.java
@@ -0,0 +1,36 @@
+package urionline.iniciante;
+
+import java.util.Scanner;
+
+/*
+#URI 1065 - PARES ENTRE CINCO NÚMEROS
+Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X,
+um valor por linha, inclusive o X, se for o caso.
+
+ENTRADA
+O arquivo de entrada contém 1 valor inteiro qualquer.
+
+SAÍDA
+Imprima todos os valores ímpares de 1 até X, inclusive X, se for o caso.
+*/
+
+public class numerosImpares {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		int X = input.nextInt();
+
+		for (int i = 1; i <= X; i++) {
+
+			if (i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+
+		input.close();
+
+	}
+
+}
