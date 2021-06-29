7f428eef2a1bb0d78b385c4d2412516759c1de6a
Luciano Viana <lucianoviana12@gmail.com>
2021-06-29 06:25:42 -0300

    Exercício Resolvido: 1071

---
+++ src/urionline/iniciante/somaDeImparesConsecutivos1.java
@@ -0,0 +1,46 @@
+package urionline.iniciante;
+
+import java.util.Scanner;
+
+/*
+#URI 1071 - SOMA DE IMPARES CONSECUTIVOS
+Leia 2 valores inteiros X e Y. A seguir, calcule e mostre a soma dos números
+impares entre eles.
+
+ENTRADA
+O arquivo de entrada contém dois valores inteiros.
+
+SAÍDA
+O programa deve imprimir um valor inteiro. Este valor é a soma dos valores ímpares
+que estão entre os valores fornecidos na entrada que deverá caber em um inteiro.
+*/
+
+public class somaDeImparesConsecutivos1 {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		int x = input.nextInt();
+		int y = input.nextInt();
+		int soma = 0;
+		int minimo, maximo = 0;
+
+		if (x < y) {
+			minimo = x;
+			maximo = y;
+		} else {
+			maximo = x;
+			minimo = y;
+		}
+
+		for (int i = minimo + 1; i < maximo; i++) {
+			if (i % 2 != 0) {
+				soma = soma + i;
+			}
+		}
+		System.out.println(soma);
+
+		input.close();
+	}
+}
